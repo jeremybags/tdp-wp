3254498227c324e1bace67959cc42a0512165408
Jeremy Bagwell <bagwellja@gmail.com>
2012-10-01 22:34:35 -0400

    updated front page - commented out page title

--- wp-content/themes/TDP-Theme/front-page.php
+++ wp-content/themes/TDP-Theme/front-page.php
@@ -27,9 +27,9 @@ Template Name: Front Page
 						<?php while ( have_posts() ) {
 							 the_post(); ?>
 							<div id="post-<?php the_ID(); ?> format-standard" <?php post_class( 'post' ); ?>>
-								<div class="post-header clearfix">
+								<!-- <div class="post-header clearfix">
 									<h1 class="post-title"><?php the_title(); ?></h1>	
-								</div> <!-- /.post-header -->						
+								</div>  --><!-- /.post-header -->						
 								<div id="content-<?php the_ID(); ?>" class="entry-content clearfix">
 									<div class="content">
 										<?php the_content(); ?>
